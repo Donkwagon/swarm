6d5718711245bccc4cedb791024d4f448795ab3e
Han <donkwagon@hotmail.com>
2017-06-19 15:31:57 -0400

    firebase auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyADsDAzimwrTKj-bWjGakZTE9IrEFsy3po",
+    authDomain: "swarm-f92be.firebaseapp.com",
+    databaseURL: "https://swarm-f92be.firebaseio.com",
+    projectId: "swarm-f92be",
+    storageBucket: "swarm-f92be.appspot.com",
+    messagingSenderId: "106479663290"
+  }
 };
