b868d24f21c6b11636b74bff462d2157acea6333
Han <donkwagon@hotmail.com>
2017-07-18 14:40:20 -0400

    admin page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { DeveloperService} from '../@core/shared/developer.service';
+import { Developer } from '../@core/classes/developer';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
+  providers: [DeveloperService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  developers: Developer[];
+
+  constructor(private developerService: DeveloperService) {
+    this.developers = [];
+  }
 
   ngOnInit() {
+    this.getDevelopers();
+  }
+
+  getDevelopers() {
+    this.developerService.getDevelopers().then(res => {
+      console.log(res);
+      res.forEach(developer => {
+        this.developers.push(developer);
+      });
+    });
   }
 
 }
