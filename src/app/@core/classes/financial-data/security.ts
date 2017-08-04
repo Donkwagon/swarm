e905d91e82a202647d3bcaaf967c483d3d44bca8
Donkw <donkwagon@hotmail.com>
2017-08-04 13:00:06 -0400

    security typing

--- src/app/@core/classes/financial-data/security.ts
+++ src/app/@core/classes/financial-data/security.ts
@@ -13,6 +13,25 @@ export class Security {
     created_at: Date;
     updated_at: Date;
 
+    earnings: any;
+    company: any;
+    quote: any;
+    news: any;
+    financials: any;
+    logo: any;
+
+    IEXListed: Boolean;
+
+    "chart": any;
+    "chart/3m": any;
+    "chart/ytd": any;
+    "chart/1m": any;
+    "chart/1y": any;
+    "chart/5y": any;
+    "chart/1d": any;
+    "chart/6m": any;
+    "chart/2y": any;
+
     constructor(){
     }
 }
